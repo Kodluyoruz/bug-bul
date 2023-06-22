4c173853cea0cc7247a889c353574707c3ddedc4
Deniz Kaparlar <65414904+denizk1@users.noreply.github.com>
2023-06-22 16:11:14 +0300

    Update multakdeger.py

--- Kolay/Python/multakdeger.py
+++ Kolay/Python/multakdeger.py
@@ -1,1 +1,7 @@
+"""
+1-Girdi parametresi olarak gelen bir tamsayının mutlak değerini geri döndüren Python fonksiyonunu, ne “if” deyimi 
+ne de math kütüphanesinden herhangi bir fonksiyonu (“abs”, “sqrt”, vs.) kullanmadan yazın. 
+Yazdığınız fonksiyonun negatif, pozitif ve 0 değerli girdi parametreleriyle doğru çalıştığından emin olun."""
 
+def mutlak(x):
+	return x*((x>0)-(x<0))
